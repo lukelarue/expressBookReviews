f218ce9572c0e0dbdd7e44b381228a38400608e2
lukelarue <larvelarue@gmail.com>
2025-07-25 23:49:30 -0400

    commit

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -62,7 +62,7 @@ regd_users.post("/login", (req, res) => {
 regd_users.put("/auth/review/:isbn", (req, res) => {
     const isbn = req.params.isbn;
     const review = req.body.review;
-    const username = req.session.authorization?.username;
+    const username = req.session.authorization['username'];
   
     if (!username) {
       return res.status(403).json({ message: "User not logged in" });
@@ -81,6 +81,31 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   
     return res.status(200).json({
       message: "Review added or updated successfully",
+      reviews: book.reviews
+    });
+  });
+
+  regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization['username'];
+  
+    if (!username) {
+      return res.status(403).json({ message: "User not logged in" });
+    }
+  
+    const book = books[isbn];
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  
+    if (!book.reviews[username]) {
+      return res.status(404).json({ message: "Review not found for this user" });
+    }
+  
+    delete book.reviews[username];
+  
+    return res.status(200).json({
+      message: "Review deleted successfully",
       reviews: book.reviews
     });
   });
